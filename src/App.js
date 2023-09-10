fd6f917e099d4ac91c7c4f953ecc3e99e6d5229f
monurajputrko <monurko99@gmail.com>
2023-09-10 23:00:34 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar />
+    </div>
+  );
+}
+
+export default App;
