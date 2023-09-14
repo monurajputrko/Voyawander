110b3ce9df034938a006efe2baa0777b1e7f0764
Monu Singh <125896049+monurajputrko@users.noreply.github.com>
2023-09-15 00:55:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 import { HolidayContextProvider } from "./Holiday/HolidayContext";
-
+import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <HolidayContextProvider>
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </HolidayContextProvider>
     </BrowserRouter>
   </Provider>
