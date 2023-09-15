6b722272718594fb7daedc7efe1e3bf01928e68f
Nishal Barman <helptechnoapps@gmail.com>
2023-09-16 01:40:45 +0530

    Payment function

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,13 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { reducer as homeReducer } from "./homepage/reducer";
+import { reducer as paymentReducer } from "./payment/reducer";
 import thunk from "redux-thunk";
 
 // combine the reducers with combineReducer-->
 
 const reducer = combineReducers({
   home: homeReducer,
+  detail: paymentReducer,
 });
 
 export const store = legacy_createStore(reducer, applyMiddleware(thunk));
