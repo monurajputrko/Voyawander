80184d72a2f67648efdd5c9512cb28f91b88fbe2
Nishal Barman <helptechnoapps@gmail.com>
2023-09-17 04:10:44 +0530

    Home

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -80,6 +80,7 @@ function Navbar({ isWhiteBackground, isOfferVisible }) {
         <div
           className={`${styles.navbar} max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4`}>
           <div
+            style={{ cursor: "pointer" }}
             onClick={() => {
               navigate("/");
             }}>
