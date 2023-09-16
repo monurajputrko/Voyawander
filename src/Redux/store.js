32f7d92598d47dac7297e446ce4f8059879eef67
Nishal Barman <helptechnoapps@gmail.com>
2023-09-16 17:23:12 +0530

    Single product page

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { reducer as homeReducer } from "./homepage/reducer";
 import { reducer as paymentReducer } from "./payment/reducer";
+import { reducer as singleProductReducer } from "./singleproduct/reducer";
 import thunk from "redux-thunk";
 
 // combine the reducers with combineReducer-->
@@ -8,6 +9,7 @@ import thunk from "redux-thunk";
 const reducer = combineReducers({
   home: homeReducer,
   detail: paymentReducer,
+  singleproduct: singleProductReducer,
 });
 
 export const store = legacy_createStore(reducer, applyMiddleware(thunk));
