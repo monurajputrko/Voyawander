b7ad785d589168f1b4e776d1c5df3d45cb9c0cc5
Nishal Barman <helptechnoapps@gmail.com>
2023-09-19 00:22:55 +0530

    Payment fixed

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -76,6 +76,12 @@ function Payment() {
     cvv: "",
   });
 
+  const [payType, setPayType] = useState(true); // using single state var, by default card
+
+  const handleRadioChange = (value) => {
+    setPayType(value === "card" ? true : false); // if card set true else false
+  };
+
   useEffect(() => {
     window.scrollTo({
       top: 0,
@@ -1192,110 +1198,50 @@ function Payment() {
                 <Text py={"10px"} fontSize={"16px"}>
                   Select your payment method:
                 </Text>
-                <Box
-                  boxShadow={"md"}
-                  border={"1px solid silver"}
-                  bg={theme ? "#191b1d" : "white"}
-                  borderRadius={"15px"}>
-                  <HStack
-                    borderBottom={"1px solid silver"}
-                    p={"15px"}
-                    borderRadius={"15px 15px 0 0 "}>
-                    <GrRadialSelected size={"20px"} color={"blue"} />
-                    <Text
-                      px={"10px"}
-                      fontSize={"16px"}
-                      fontWeight={"400"}
-                      color={theme ? "white" : "blackAlpha.800"}>
-                      Pay by card
-                    </Text>
-                    <Spacer />
-                    <Image
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLVIBJ-2rpBUh0gMNNjNhM759BiZ4ZEEC9BQ&usqp=CAU"
-                      width="5%"
-                      height="5%"
-                      mt="22px"
-                      ml="30px"
-                    />
-                    <Image
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkrA_6LJ0DJucgHSZDOYO8v9n7dGNiJ6OPnw&usqp=CAU"
-                      width="7%"
-                      height="7%"
-                      mt="16px"
-                      ml="0px"
-                    />
-                    <Image
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSapt6bCfFKVEeVMsp5NRz4iOLHcGBqzPtPWw&usqp=CAU"
-                      width="4%"
-                      height="4%"
-                      mt="20px"
-                      mr="20px"
-                    />
-                  </HStack>
-
-                  <Box p={"20px 15px 30px 15px"}>
-                    <Flex direction={"column"}>
-                      <label
-                        style={{
-                          marginBottom: "-10px",
-                          marginLeft: "15px",
-                          fontSize: "14px",
-                          fontWeight: "700",
-                          backgroundColor: theme ? "#191b1d" : "white",
-                          zIndex: "1",
-                          width: "125px",
-                          paddingLeft: "5px",
-                        }}>
-                        Cardholder Name
-                      </label>
-                      <Input
-                        className="inpt"
-                        autoComplete="on"
-                        onChange={(e) => {
-                          setformdata({
-                            ...formdata,
-                            c_holder: e.target.value,
-                          });
-                        }}
-                        w={{ base: "80%", md: "45%", lg: "45%" }}
-                        size="lg"
-                        border={"1px solid silver"}
+                {payType && (
+                  <Box
+                    boxShadow={"md"}
+                    border={"1px solid silver"}
+                    bg={theme ? "#191b1d" : "white"}
+                    borderRadius={"15px"}>
+                    <HStack
+                      borderBottom={"1px solid silver"}
+                      p={"15px"}
+                      borderRadius={"15px 15px 0 0 "}>
+                      <GrRadialSelected size={"16px"} color={"blue"} />
+                      <Text
+                        px={"10px"}
+                        fontSize={"16px"}
+                        fontWeight={"400"}
+                        color={theme ? "white" : "blackAlpha.800"}>
+                        Pay by card
+                      </Text>
+                      <Spacer />
+                      <Image
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLVIBJ-2rpBUh0gMNNjNhM759BiZ4ZEEC9BQ&usqp=CAU"
+                        width="5%"
+                        height="5%"
+                        mt="22px"
+                        ml="30px"
                       />
-                    </Flex>
-                    <Flex direction={"column"} my={"20px"}>
-                      <label
-                        style={{
-                          marginBottom: "-10px",
-                          marginLeft: "15px",
-                          fontSize: "14px",
-                          fontWeight: "700",
-                          backgroundColor: theme ? "#191b1d" : "white",
-                          zIndex: "1",
-                          width: "110px",
-                          paddingLeft: "5px",
-                        }}>
-                        Card Number *
-                      </label>
-                      <Input
-                        className="inpt"
-                        autoComplete="on"
-                        onChange={(e) => {
-                          setformdata({
-                            ...formdata,
-                            c_number: e.target.value,
-                          });
-                        }}
-                        w={{ base: "80%", md: "45%", lg: "45%" }}
-                        size="lg"
-                        border={"1px solid silver"}
-                        placeholder="**** **** **** ****"
+                      <Image
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkrA_6LJ0DJucgHSZDOYO8v9n7dGNiJ6OPnw&usqp=CAU"
+                        width="7%"
+                        height="7%"
+                        mt="16px"
+                        ml="0px"
                       />
-                    </Flex>
+                      <Image
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSapt6bCfFKVEeVMsp5NRz4iOLHcGBqzPtPWw&usqp=CAU"
+                        width="4%"
+                        height="4%"
+                        mt="20px"
+                        mr="20px"
+                      />
+                    </HStack>
 
-                    <Flex direction={{ base: "column", md: "row", lg: "row" }}>
-                      <Flex
-                        direction={"column"}
-                        w={{ base: "60%", md: "20%", lg: "20%" }}>
+                    <Box p={"20px 15px 30px 15px"}>
+                      <Flex direction={"column"}>
                         <label
                           style={{
                             marginBottom: "-10px",
@@ -1304,30 +1250,26 @@ function Payment() {
                             fontWeight: "700",
                             backgroundColor: theme ? "#191b1d" : "white",
                             zIndex: "1",
-                            width: "95px",
-                            paddingLeft: "3px",
+                            width: "125px",
+                            paddingLeft: "5px",
                           }}>
-                          Expiry Date *
+                          Cardholder Name
                         </label>
                         <Input
                           className="inpt"
+                          autoComplete="on"
                           onChange={(e) => {
                             setformdata({
                               ...formdata,
-                              exp_day: e.target.value,
+                              c_holder: e.target.value,
                             });
                           }}
-                          w={"100%"}
+                          w={{ base: "80%", md: "45%", lg: "45%" }}
                           size="lg"
                           border={"1px solid silver"}
-                          placeholder="MM / YY"
                         />
                       </Flex>
-                      <Flex
-                        direction={"column"}
-                        w={{ base: "60%", md: "20%", lg: "20%" }}
-                        ml={{ base: "0%", md: "5%", lg: "5%" }}
-                        mt={{ base: "15px", md: "0px", lg: "0px" }}>
+                      <Flex direction={"column"} my={"20px"}>
                         <label
                           style={{
                             marginBottom: "-10px",
@@ -1336,25 +1278,107 @@ function Payment() {
                             fontWeight: "700",
                             backgroundColor: theme ? "#191b1d" : "white",
                             zIndex: "1",
-                            width: "45px",
-                            paddingLeft: "3px",
+                            width: "110px",
+                            paddingLeft: "5px",
                           }}>
-                          CVV *
+                          Card Number *
                         </label>
                         <Input
                           className="inpt"
+                          autoComplete="on"
                           onChange={(e) => {
-                            setformdata({ ...formdata, cvv: e.target.value });
+                            setformdata({
+                              ...formdata,
+                              c_number: e.target.value,
+                            });
                           }}
-                          w={"100%"}
+                          w={{ base: "80%", md: "45%", lg: "45%" }}
                           size="lg"
                           border={"1px solid silver"}
-                          placeholder="1 2 3"
+                          placeholder="**** **** **** ****"
                         />
                       </Flex>
-                    </Flex>
+
+                      <Flex
+                        direction={{ base: "column", md: "row", lg: "row" }}>
+                        <Flex
+                          direction={"column"}
+                          w={{ base: "60%", md: "20%", lg: "20%" }}>
+                          <label
+                            style={{
+                              marginBottom: "-10px",
+                              marginLeft: "15px",
+                              fontSize: "14px",
+                              fontWeight: "700",
+                              backgroundColor: theme ? "#191b1d" : "white",
+                              zIndex: "1",
+                              width: "95px",
+                              paddingLeft: "3px",
+                            }}>
+                            Expiry Date *
+                          </label>
+                          <Input
+                            className="inpt"
+                            onChange={(e) => {
+                              setformdata({
+                                ...formdata,
+                                exp_day: e.target.value,
+                              });
+                            }}
+                            w={"100%"}
+                            size="lg"
+                            border={"1px solid silver"}
+                            placeholder="MM / YY"
+                          />
+                        </Flex>
+                        <Flex
+                          direction={"column"}
+                          w={{ base: "60%", md: "20%", lg: "20%" }}
+                          ml={{ base: "0%", md: "5%", lg: "5%" }}
+                          mt={{ base: "15px", md: "0px", lg: "0px" }}>
+                          <label
+                            style={{
+                              marginBottom: "-10px",
+                              marginLeft: "15px",
+                              fontSize: "14px",
+                              fontWeight: "700",
+                              backgroundColor: theme ? "#191b1d" : "white",
+                              zIndex: "1",
+                              width: "45px",
+                              paddingLeft: "3px",
+                            }}>
+                            CVV *
+                          </label>
+                          <Input
+                            className="inpt"
+                            onChange={(e) => {
+                              setformdata({ ...formdata, cvv: e.target.value });
+                            }}
+                            w={"100%"}
+                            size="lg"
+                            border={"1px solid silver"}
+                            placeholder="1 2 3"
+                          />
+                        </Flex>
+                      </Flex>
+                    </Box>
                   </Box>
-                </Box>
+                )}
+                {/* {pay by card} */}
+                <Box mb={"40px"}></Box>
+                <RadioGroup onChange={handleRadioChange}>
+                  {!payType && (
+                    <HStack direction="column">
+                      <Radio value="card">Pay by card</Radio>
+                    </HStack>
+                  )}
+                  <HStack direction="column">
+                    <Radio value="razorpay" colorScheme="red">
+                      Razor Pay
+                    </Radio>
+                  </HStack>
+                </RadioGroup>
+
                 <Box mt={"20px"}>
                   <Flex>
                     <Checkbox
@@ -1384,60 +1408,89 @@ function Payment() {
                     </Text>
                   </Flex>
                 </Box>
-                <Button
-                  isDisabled={!chk}
-                  w={"100%"}
-                  onClick={() => {
-                    if (
-                      !formdata.first_N ||
-                      !formdata.last_N ||
-                      !formdata.title ||
-                      !formdata.email ||
-                      !formdata.phone ||
-                      !formdata.day ||
-                      !formdata.month ||
-                      !formdata.year ||
-                      !formdata.gender ||
-                      !formdata.nationality ||
-                      !formdata.c_holder ||
-                      !formdata.c_number ||
-                      !formdata.exp_day ||
-                      !formdata.cvv
-                    ) {
-                      toast({
-                        title: "Please fill all details",
-                        status: "warning",
-                        position: "top",
-                        duration: 3000,
-                        isClosable: true,
-                      });
-                    } else {
-                      onOpen();
-                      toast({
-                        description: "Your OTP is 5637",
-                        status: "success",
-                        position: "top",
-                        duration: 3000,
-                        isClosable: true,
-                      });
-                    }
-                  }}
-                  colorScheme="none"
-                  bg={"#008cc9"}
-                  py={"30px"}
-                  mt={"30px"}>
-                  Book Spaces
-                </Button>
-                <Button
-                  w={"100%"}
-                  isDisabled={!chk}
-                  onClick={handlePayment}
-                  colorScheme="none"
-                  bg={"#008cc9"}
-                  py={"30px"}
-                  mt={"30px"}>
-                  Pay Now
-                </Button>
+                <Box className="btn">
+                  {payType && (
+                    <Button
+                      isDisabled={!chk}
+                      w={"100%"}
+                      onClick={() => {
+                        if (
+                          !formdata.first_N ||
+                          !formdata.last_N ||
+                          !formdata.title ||
+                          !formdata.email ||
+                          !formdata.phone ||
+                          !formdata.day ||
+                          !formdata.month ||
+                          !formdata.year ||
+                          !formdata.gender ||
+                          !formdata.nationality ||
+                          !formdata.c_holder ||
+                          !formdata.c_number ||
+                          !formdata.exp_day ||
+                          !formdata.cvv
+                        ) {
+                          toast({
+                            title: "Please fill all details",
+                            status: "warning",
+                            position: "top",
+                            duration: 3000,
+                            isClosable: true,
+                          });
+                        } else {
+                          toast({
+                            title: "Your OTP is : 5637",
+                            position: "top",
+                            status: "success",
+                            duration: 3000,
+                            isClosable: true,
+                          });
+                          onOpen();
+                        }
+                      }}
+                      colorScheme="none"
+                      bg={"#1ca0e3"}
+                      py={"30px"}
+                      mt={"30px"}>
+                      Book Spaces
+                    </Button>
+                  )}
+                </Box>
+                {/* onClick={handlePayment} */}
+                {!payType && (
+                  <Button
+                    isDisabled={!chk}
+                    w={"100%"}
+                    onClick={() => {
+                      if (
+                        !formdata.first_N ||
+                        !formdata.last_N ||
+                        !formdata.email ||
+                        !formdata.phone ||
+                        !formdata.day ||
+                        !formdata.month ||
+                        !formdata.year ||
+                        !formdata.gender ||
+                        !formdata.nationality
+                      ) {
+                        toast({
+                          title: "Please fill all details",
+                          status: "warning",
+                          position: "top",
+                          duration: 3000,
+                          isClosable: true,
+                        });
+                      } else {
+                        handlePayment();
+                      }
+                    }}
+                    colorScheme="none"
+                    bg={"#008cc9"}
+                    py={"30px"}
+                    mt={"30px"}>
+                    Pay Now
+                  </Button>
+                )}
                 <Modal isOpen={isOpen}>
                   <ModalOverlay />
                   <ModalContent w={"350px"} top={"22%"} py={"30px"}>
@@ -1526,7 +1579,6 @@ function Payment() {
                     </Button>
                   </ModalContent>
                 </Modal>
-
                 <Box w={"50%"} m={"auto"} textAlign={"center"} mt={"20px"}>
                   <Text fontSize={"13px"} fontWeight={"700"}>
                     No booking fees!{" "}
